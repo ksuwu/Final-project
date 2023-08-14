09c0bcb7728b5aadcec27404b67b22545db50065
f <f>
2023-08-14 17:18:50 +0300

    Initial Commit

---
+++ Final project/Final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_projectApp.swift
+//  Final project
+//
+//  Created by Kseniia on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
